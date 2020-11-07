77cec9ac1511ae17b2b5822ef3dbfa35ab2285d1
Leroy Smith <leroys2019@gmail.com>
2020-11-07 01:07:46 -0800

    BinarySearch fixing

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -5,7 +5,20 @@ public class BinarySearch {
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
-        // homework
-        return -1;  // place holder
+        int[] nData = null;
+        int size = data.length;
+         if (size > 0){
+             nData = new int[size];
+             int i;
+             for (i = 0; i < size; i++){
+                 nData[i] = data[i];
+             }
+             if (data[data.length-1] == target){
+                 return size-1;
+             }else if (data[data.length-1] != target){
+                 return binarySearch(nData, target);//recursive
+             }
+         }
+         return -1;//Returns a -1 if the target isn't found or iff the array is empty.
     }
 }
