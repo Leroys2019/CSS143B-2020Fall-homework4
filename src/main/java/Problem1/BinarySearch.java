cb97670f53fa4a2ac62c4668e8bdcf55afff0438
Leroy Smith <leroys2019@gmail.com>
2020-11-07 01:40:05 -0800

    BinarySearch Test Passes Permutation still needs work

--- src/main/java/Problem1/BinarySearch.java
+++ src/main/java/Problem1/BinarySearch.java
@@ -5,20 +5,19 @@ public class BinarySearch {
     // Do not change signature (return type, function name, parameter types)
     // Add a helper function for recursion calls
     public static int binarySearch(int[] data, int target) {
-        int[] nData = null;
+        int[] nData = null;//sets the new array to be empty
         int size = data.length;
-         if (size > 0){
-             nData = new int[size];
-             int i;
-             for (i = 0; i < size; i++){
-                 nData[i] = data[i];
-             }
-             if (data[data.length-1] == target){
-                 return size-1;
-             }else if (data[data.length-1] != target){
-                 return binarySearch(nData, target);//recursive
-             }
-         }
-         return -1;//Returns a -1 if the target isn't found or iff the array is empty.
+        if(size > 0) {
+            nData = new int[size - 1];
+            for (int i = 0; i < nData.length; i++) {//for-loop that fills new array with elements from array within the parameter
+                nData[i] = data[i];
+            }
+            if (data[size - 1] == target) {
+                return size - 1;
+            } else {
+                return binarySearch(nData, target);//recursion of binarySearch
+            }
+        }
+        return -1;//returns -1 if the array is empty or if the target isn't found.
     }
 }
--- src/main/java/Problem1/Permutation.java
+++ src/main/java/Problem1/Permutation.java
@@ -6,6 +6,16 @@ import java.util.List;
 public class Permutation {
     public static List<List<Integer>> permute(int[] nums) {
         // homework
-        return new ArrayList<>(); // place holder
+        List<List<Integer>> results = new ArrayList<>();
+        findPer(nums, 0, results);
+        return results;
+    }
+    public static void findPer(int[] nums, int position, List<List<Integer>> results) {
+        if(position == nums.length -1){
+            List<Integer> nPermute = new ArrayList<Integer>();
+            for(int i =0; i<nums.length;i++){
+                nPermute.add(nums[i]);
+            }
+        }
     }
 }
